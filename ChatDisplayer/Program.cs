27727067094c27c2971e55f611ab27b81ef9c143
Chutipong Roupkhom <c1000672@THAIBEV.COM>
2017-01-26 10:32:31 +0700

    init and final

---
+++ ChatDisplayer/Program.cs
@@ -0,0 +1,59 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+using System.Threading;
+
+namespace ChatDisplayer
+{
+    internal class Program
+    {
+        private static ConnectionFactory _factory;
+
+        private static IConnection _connection;
+        private static IModel _channel;
+
+        private static string _username = "m";
+        private static string _password = "123123";
+        private static readonly string _queueName = "RealtimeQueue";
+        private static readonly string _queueName1 = "RealtimeQueue1";
+        private static readonly string _virtualHost = "Realtime";
+
+        private static void Main(string[] args)
+        {
+            DeQueue();
+        }
+
+        public static void DeQueue()
+        {
+            _factory = new ConnectionFactory
+            {
+                HostName = "localhost",
+                UserName = _username,
+                Password = _password,
+                VirtualHost = _virtualHost,
+                AutomaticRecoveryEnabled = true,
+                NetworkRecoveryInterval = TimeSpan.FromSeconds(10)
+            };
+
+            _connection = _factory.CreateConnection();
+            _channel = _connection.CreateModel();
+
+            var consumer = new EventingBasicConsumer(_channel);
+            consumer.Received += (ch, ea) =>
+            {
+                var body = ea.Body;
+                var message = Encoding.UTF8.GetString(body);
+
+                Console.WriteLine(message + " : " + ea.DeliveryTag.ToString());
+                Thread.Sleep(50);
+
+                // Done stamping to RabbitMQ server
+                _channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
+            };
+
+            _channel.BasicConsume(queue: _queueName, noAck: false, consumer: consumer);
+            _channel.BasicConsume(queue: _queueName1, noAck: false, consumer: consumer);
+        }
+    }
+}
---
+++ ConsoleLineApp/Program.cs
@@ -0,0 +1,91 @@
+﻿using Faker;
+using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace ConsoleLineApp
+{
+    public class Program
+    {
+        private static ConnectionFactory _factory;
+
+        private static IConnection _connection;
+        private static IModel _channel;
+
+        private static string _username = "m";
+        private static string _password = "123123";
+        private static readonly string _queueName = "RealtimeQueue";
+
+        private static readonly string _queueName1 = "RealtimeQueue1";
+        private static readonly string _exchangeName = "RealtimeExchange";
+
+        private static readonly string _routingKey = "routingkey";
+        private static readonly string _routingKey1 = "routingkey1";
+        private static readonly string _virtualHost = "Realtime";
+
+        private static void Main(string[] args)
+        {
+            DelareQueue();
+            BindQueue();
+
+            // Loopgin to random string and enqueue first
+            for (var i = 0; i < 101; i++)
+            {
+                var randomString = "[" + i + "] : " + Lorem.Sentence(10);
+                Console.WriteLine("Number : [{0}] with :" + randomString, i);
+
+                var obj = Encoding.ASCII.GetBytes(randomString);
+
+                EnQueue(obj);
+            }
+
+            // Loopgin to random string and enqueue seconde
+            for (var i = 0; i < 100; i++)
+            {
+                var randomString = "[" + i + "] : " + Lorem.Sentence(10);
+                Console.WriteLine("Number : [{0}] with :" + randomString, i);
+
+                var obj = Encoding.ASCII.GetBytes(randomString);
+
+                EnQueue1(obj);
+            }
+        }
+
+        public static void DelareQueue()
+        {
+            _factory = new ConnectionFactory
+            {
+                HostName = "localhost",
+                UserName = _username,
+                Password = _password,
+                VirtualHost = _virtualHost,
+                AutomaticRecoveryEnabled = true,
+                NetworkRecoveryInterval = TimeSpan.FromSeconds(10)
+            };
+
+            _connection = _factory.CreateConnection();
+
+            _channel = _connection.CreateModel();
+
+            _channel.QueueDeclare(queue: _queueName, durable: true, exclusive: false, autoDelete: false, arguments: null);
+            _channel.QueueDeclare(queue: _queueName1, durable: true, exclusive: false, autoDelete: false, arguments: null);
+        }
+
+        private static void BindQueue()
+        {
+            _channel.QueueBind(queue: _queueName, exchange: _exchangeName, routingKey: _routingKey, arguments: null);
+            _channel.QueueBind(queue: _queueName1, exchange: _exchangeName, routingKey: _routingKey1, arguments: null);
+        }
+
+        public static void EnQueue(byte[] line)
+        {
+            _channel.BasicPublish(exchange: _exchangeName, routingKey: _routingKey, mandatory: false, basicProperties: null, body: line);
+        }
+
+        private static void EnQueue1(byte[] line)
+        {
+            _channel.BasicPublish(exchange: _exchangeName, routingKey: _routingKey1, mandatory: false,
+                basicProperties: null, body: line);
+        }
+    }
+}
